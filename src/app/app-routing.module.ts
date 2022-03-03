c034b00df0d2fec2af585967fdd439b42cde4724
luongvietvinh <luongvietvinh@gmail.com>
2022-03-03 10:36:53 +0700

    v

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {BookComponent} from "./book/book.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'vinh', loadChildren:()=> import('./book/book.module').then(moude => moude.BookModule)},
+  { path: 'book', component: BookComponent },
+  { path: '', component: BookComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BookComponent } from './book/book.component';
+import { CreateComponent } from './book/create/create.component';
+import { EditComponent } from './book/edit/edit.component';
+import { DetailComponent } from './book/detail/detail.component';
+import {HttpClientModule} from "@angular/common/http";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatIconModule} from '@angular/material/icon';
+import {MatTableModule} from "@angular/material/table";
+import {MatButtonModule} from "@angular/material/button";
+import {BookModule} from "./book/book.module";
+import {MatInputModule} from "@angular/material/input";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BookComponent,
+    CreateComponent,
+    EditComponent,
+    DetailComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule, HttpClientModule, BrowserAnimationsModule,
+    MatIconModule, MatTableModule, MatButtonModule, BookModule, MatInputModule,
+    FormsModule,ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
